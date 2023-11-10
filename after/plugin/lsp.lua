753d1c0120891a680650194b9adc91817518881c
Peter Nirschl <peter.nirschl@gmail.com>
2023-11-10 23:36:41 +0100

    install Go LSP
    Signed-off-by: Peter Nirschl <peter.nirschl@gmail.com>

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,7 +21,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'tsserver','eslint', 'rust_analyzer'},
+  ensure_installed = {'tsserver','eslint', 'rust_analyzer', 'gopls'},
   handlers = {
     lsp.default_setup,
   }
