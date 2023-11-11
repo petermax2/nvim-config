4e6d35f3193dd6fef555b71c1beeb47a68fc88a5
Peter Nirschl <peter.nirschl@gmail.com>
2023-11-11 18:26:59 +0100

    update color schema
    Signed-off-by: Peter Nirschl <peter.nirschl@gmail.com>

--- lua/nirpet/colors.lua
+++ lua/nirpet/colors.lua
@@ -1,3 +1,3 @@
-vim.cmd.colorscheme "catppuccin-mocha"
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.cmd.colorscheme "catppuccin-macchiato"
+--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
