ab900807a32eb61fcdee02038cfd01d4e6e80975
Peter Nirschl <peter.nirschl@gmail.com>
2023-12-03 15:48:08 +0100

    install haskell-tools
    Signed-off-by: Peter Nirschl <peter.nirschl@gmail.com>

--- lua/nirpet/packer.lua
+++ lua/nirpet/packer.lua
@@ -15,6 +15,7 @@ return require('packer').startup(function(use)
 	use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} }
 	use {'mbbill/undotree'}
 	use {'tpope/vim-fugitive'}
+	use {'haskell-tools/haskell-tools'}
 
 	use {
 		'VonHeikemen/lsp-zero.nvim',
